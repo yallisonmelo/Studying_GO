7105250627f70835bcaa6b8a3b702b3266bf5c5b
yallisonmelo <yallisonmelo@gmail.com>
2023-07-20 15:05:18 -0300

    Studing Go - Interface Assertions

--- Interface/interface.go
+++ Interface/interface.go
@@ -5,6 +5,14 @@ import "fmt"
 func main() {
 	printers := []Printer{Epson{}, HP{}}
 	for _, printer := range printers {
+		switch printer.(type) {
+		case Epson:
+			fmt.Println("Epson printer")
+		case HP:
+			fmt.Println("HP printer")
+		default:
+			fmt.Println("Unknown printer")
+		}
 		fmt.Println(printer.printing())
 	}
 }
